01207c1869d95d0e283ac8958c9174de6647344c
Praveen <praveenyarnal1991@gmail.com>
2021-05-16 14:14:58 +0530

    added notes for auto retrying the failed TC's

--- RetryLogic/src/Analyzer/RetryTransformer.java
+++ RetryLogic/src/Analyzer/RetryTransformer.java
@@ -14,5 +14,13 @@ public class RetryTransformer implements IAnnotationTransformer{
 		annotation.setRetryAnalyzer(RetryAnalyzer.class);
 	}
 
-	
 }
+//  Steps to run the failed test cases 
+//1. Create a RetryAnalyzer class which implements IRetryAnalyzer Interface
+//2. Include retry overridden method in RetryAnalyzer class. The Method has The object of ITestResult 
+//& logic with maxLimit & Count  Finaly it returns the boolean value
+//3. Register the RetryAnalyzer class in RetryTrnasformer class
+//   using overridden method setRetryAnalyzer method.
+//4. use the attribute retryAnalyzer for @Test and set the RetryAnalyzer class 
+
+//3. 
